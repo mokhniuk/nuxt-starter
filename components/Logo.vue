8cf0318f24d4e3deab9aa90789822cdf64a9db5e
Oleg Mokhniuk <oleg.mokhniuk@gmail.com>
2018-01-17 21:14:58 +0100

    Init nuxt starter

---
+++ components/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="site-logo">Nuxt Starter</h1>
+</template>
+
+<style>
+  .site-logo {
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="container">
+      <logo/>
+    </div>
+  </main>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      Logo
+    }
+  }
+</script>
+
+<style>
+  .container {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 16px;
+    box-sizing: content-box;
+  }
+</style>
