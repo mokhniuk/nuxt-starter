e3657bb378a34c56e2aa76099ebe41b8eb19b280
Oleg Mokhniuk <oleg.mokhniuk@gmail.com>
2018-01-20 21:59:32 +0100

    Move navigation to separate component

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -2,30 +2,19 @@
   <header class="site-header">
     <div class="container">
       <logo/>
-      <nav class="main-nav">
-        <ul class="main-nav__list">
-          <li class="main-nav__list-item">
-            <nuxt-link class="main-nav__link" :to="$i18n.path('')" exact>{{ $t('links.home') }}</nuxt-link>
-          </li>
-          <li class="main-nav__list-item">
-            <nuxt-link class="main-nav__link" :to="$i18n.path('about')" exact>{{ $t('links.about') }}</nuxt-link>
-          </li>
-          <li class="main-nav__list-item main-nav__list-item--lang">
-            <nuxt-link class="main-nav__link" v-if="$i18n.locale === 'en'" :to="`/ua` + $route.fullPath" active-class="none" exact>{{ $t('links.ukrainian') }}</nuxt-link>
-            <nuxt-link class="main-nav__link" v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" active-class="none" exact>{{ $t('links.english') }}</nuxt-link>
-          </li>
-        </ul>
-      </nav>
+      <site-nav/>
     </div>
   </header>
 </template>
 
 <script>
   import Logo from '~/components/Logo.vue'
+  import SiteNav from '~/components/SiteNav.vue'
 
   export default {
     components: {
-      Logo
+      Logo,
+      SiteNav
     }
   }
 </script>
@@ -40,44 +29,10 @@
     text-align: center;
   }
 
-  .main-nav {
-    display: inline-block;
-    vertical-align: middle;
-  }
-
+  
   .site-logo {
     margin-bottom: 1em;
     width: 100%;
-  }
-
-  .main-nav__list {
-    display: block;
-    list-style-type: none;
-  }
-
-  .main-nav__list-item {
-    display: inline-block;
-    margin-right: 2em;
-
-    &:last-of-type,
-    &:last-child {
-      margin-right: 0
-    }
-  }
-
-  .main-nav__list-item--lang {
-    float: right;
-  }
-
-  .main-nav__link {
-    text-decoration: none;
-    color: $black;
-    border-bottom: 1px solid transparent;
-
-    &:hover,
-    &.nuxt-link-active {
-      border-color: currentcolor;
-    }
   }
 
   @media screen and(min-width: 600px) {
---
+++ components/SiteNav.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="main-nav">
+    <ul class="main-nav__list">
+      <li class="main-nav__list-item">
+        <nuxt-link class="main-nav__link" :to="$i18n.path('')" exact>{{ $t('links.home') }}</nuxt-link>
+      </li>
+      <li class="main-nav__list-item">
+        <nuxt-link class="main-nav__link" :to="$i18n.path('about')" exact>{{ $t('links.about') }}</nuxt-link>
+      </li>
+      <li class="main-nav__list-item">
+        <nuxt-link class="main-nav__link" v-if="$i18n.locale === 'en'" :to="`/ua` + $route.fullPath" active-class="none" exact>{{ $t('links.ukrainian') }}</nuxt-link>
+        <nuxt-link class="main-nav__link" v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" active-class="none" exact>{{ $t('links.english') }}</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss">
+  @import "../assets/styles/base/variables";
+  @import "../assets/styles/settings";
+
+  .main-nav {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .main-nav__list {
+    display: block;
+    list-style-type: none;
+  }
+
+  .main-nav__list-item {
+    display: inline-block;
+    margin-right: 2em;
+
+    &:last-of-type,
+    &:last-child {
+      margin-right: 0
+    }
+  }
+
+  .main-nav__link {
+    text-decoration: none;
+    color: $black;
+    border-bottom: 1px solid transparent;
+
+    &:hover,
+    &.nuxt-link-active {
+      border-color: currentcolor;
+    }
+  }
+</style>
