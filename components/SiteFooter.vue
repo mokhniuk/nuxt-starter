5f34d8bbeb78e679f5b6edb7d6588918b5de16bf
Oleg Mokhniuk <oleg.mokhniuk@gmail.com>
2018-02-03 20:33:27 +0100

    add footer component

---
+++ components/SiteFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="site-footer" role="contentinfo">
+    <div class="container">
+      <p>© <span class="year"></span></p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  
+</script>
+
+<style>
+  .site-footer {}
+</style>
